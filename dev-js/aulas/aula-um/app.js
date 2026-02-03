cbb1ec91a751220901cfbc2116033d3e1c3fcb6f
miguelzackk <mzsilvaw@gmail.com>
2026-02-03 19:33:56 -0300

    feat: finish aula-um.

---
+++ dev-js/aulas/aula-um/app.js
@@ -0,0 +1,10 @@
+// let nome = prompt("Digite seu nome")
+// let idade = prompt("Digite a sua idade")
+
+let nome = "Miguel"
+let idade = 17
+
+console.log(`Seja Bem-Vindo(a) ${nome}, sua idade é ${idade}`)
+
+console.log("A soma de 7 + 3 é: " + (7 +3)
+)
