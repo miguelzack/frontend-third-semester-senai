3fcf6010e59ce31e5f2cbaa7aef51db92dc073fb
miguelzackk <mzsilvaw@gmail.com>
2026-02-03 19:56:40 -0300

    feat: finish aula-dois.

--- dev-js/aulas/aula-dois/tipos.js
+++ dev-js/aulas/aula-dois/tipos.js
@@ -2,11 +2,19 @@ var nome = "25"
 var idade = 17
 var vivoBoolean = true
 var vivo = 1
+let naosei
+let end = null
+let numerogrande = BigInt(1)
+let symbol = Symbol(1)
 
 console.log(typeof nome)
 console.log(typeof idade)
 console.log(typeof vivoBoolean)
 console.log(typeof vivo)
+console.log(typeof naosei)
+console.log(end)
+console.log(typeof numerogrande)
+console.log(typeof symbol)
 
 console.log(" ")
 
@@ -19,4 +27,7 @@ console.log(typeof idadeConvertida)
 var vivoConvertido = Boolean(vivo)
 console.log(typeof vivoConvertido)
 
-//Falta o professor colocar o que eu tenho que explicar
+//Valor primitivo é o tipo mais simples de variável, no qual armazena um único valor.
+// Ou seja, defini que o valor para nome é Miguel, quando eu colocar nome = Alberto, ele não vai adicionar como na lista, ele vai substituir o valor.
+
+//Valor por referência é objeto e array. Ao copiar o mesmo, não é copiado o valor, mas simo endereço na memória.
