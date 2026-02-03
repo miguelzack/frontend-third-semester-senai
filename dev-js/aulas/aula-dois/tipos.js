681ff139ca61e048d5f0f478c8244dacbc58476e
miguelzackk <mzsilvaw@gmail.com>
2026-02-03 19:46:20 -0300

    feat: add aula-dois.

---
+++ dev-js/aulas/aula-dois/tipos.js
@@ -0,0 +1,22 @@
+var nome = "25"
+var idade = 17
+var vivoBoolean = true
+var vivo = 1
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof vivoBoolean)
+console.log(typeof vivo)
+
+console.log(" ")
+
+var nomeConvertido = Number(nome)
+console.log(typeof nomeConvertido)
+
+var idadeConvertida = String(idade)
+console.log(typeof idadeConvertida)
+
+var vivoConvertido = Boolean(vivo)
+console.log(typeof vivoConvertido)
+
+//Falta o professor colocar o que eu tenho que explicar
