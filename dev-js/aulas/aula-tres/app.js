d990b5dba63dd8af10f3ad068766856e23ed7a8e
miguelzackk <mzsilvaw@gmail.com>
2026-02-04 20:37:51 -0300

    feat: finish aula-tres

---
+++ dev-js/aulas/aula-tres/app.js
@@ -0,0 +1,15 @@
+let nome = "Miguel"
+let idade = 17
+let cidade = "São Paulo"
+
+let num1 = 2
+let num2 = 2
+
+console.log("Meu nome é " + nome + ", tenho " + idade + " anos e sou de " + cidade + ".")
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+console.log(`Meu nome é ${nome}, tenho ${idade} e sou de ${cidade}.`)
