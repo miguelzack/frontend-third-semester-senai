7e2894b95a5ecc03e88add127bad5e9623166460
miguelzackk <mzsilvaw@gmail.com>
2026-02-04 22:15:45 -0300

    feat: finish aula-quatro

---
+++ dev-js/aulas/aula-quatro/app2.js
@@ -0,0 +1,11 @@
+let nota = 800
+
+if (nota >= 9) {
+    console.log("Excelente")
+} else if (nota >= 7) {
+    console.log("Bom")
+} else if (nota >= 5) {
+    console.log("Regular")
+} else {
+    console.log("Reprovado.")
+}
