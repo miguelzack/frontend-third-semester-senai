e0333fd4010c196666e2a911ed68ec61289b0586
miguelzackk <mzsilvaw@gmail.com>
2026-03-25 09:57:36 -0300

    feat: finish firts component

---
+++ react-component/src/App.jsx
@@ -0,0 +1,15 @@
+import Breadcrumb from "./components/breadcrumb/breadcrumb";
+
+function App() {
+    return (<>
+        <h1>Componentes em react</h1>
+        <p>Componente com CSS puro:</p>
+        <Breadcrumb/>
+        <p>Componente com modules:</p>
+
+        <p>Componente com styled-components:</p>
+
+    </>);
+}
+
+export default App;
---
+++ react-component/src/components/breadcrumb/breadcrumb.jsx
@@ -0,0 +1,14 @@
+import "./breadcrumb.css"
+//css puro
+
+export default function Breadcrumb() {
+    return (
+        <nav>
+            <ol>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li>Settings</li>
+            </ol>
+        </nav>
+    )
+}
