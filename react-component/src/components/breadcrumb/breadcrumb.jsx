e81753b293e23bfa86e9f6f04e8e318067767168
miguelzackk <mzsilvaw@gmail.com>
2026-03-31 10:02:24 -0300

    feat: add js in breadcrumb component

--- react-component/src/components/breadcrumb/breadcrumb.jsx
+++ react-component/src/components/breadcrumb/breadcrumb.jsx
@@ -1,14 +1,54 @@
 import "./breadcrumb.css"
-//css puro
+// css puro
+import {useState} from "react"
 
 export default function Breadcrumb() {
-    return (
+    const [page, setPage] = useState("Settings")
+
+    const handleClick = (e, newPage) => {
+        e.preventDefault()
+        setPage(newPage)
+    }
+
+    return (<>
         <nav>
             <ol>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Profile</a></li>
-                <li>Settings</li>
+                <li>
+                    <a
+                        href="#"
+                        onClick={(e) => handleClick(e, "Home")}
+                        className={page === "Home" ? "active" : ""}
+                    >
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="#"
+                        onClick={(e) => handleClick(e, "Profile")}
+                        className={page === "Profile" ? "active" : ""}
+                    >
+                        Profile
+                    </a>
+                </li>
+
+                <li
+                    c
+                >
+                    <a
+                        href="#"
+                        className={page === "Settings" ? "active" : ""}
+                        onClick={() => setPage("Settings")}
+                    >
+                        Settings
+                    </a>
+                </li>
             </ol>
         </nav>
-    )
+
+        <div>
+            <h1>{page} page</h1>
+        </div>
+    </>)
 }
