ef2fffc2d688c6d2b076c4f54a7d278f1aba7e36
miguelzackk <mzsilvaw@gmail.com>
2026-04-07 10:53:01 -0300

    fet: finish main in 'rickAndMorty-API'

--- rickAndMorty-API/src/App.jsx
+++ rickAndMorty-API/src/App.jsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import './App.css'
 import {Header} from "./components/Header/";
+import {Main} from "./components/Main/index.jsx";
 
 function App() {
     return (<>
         <Header/>
+        <Main/>
     </>)
 }
 
