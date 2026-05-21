0e603fefac0656e799ad8463284e2ac064d7d6d7
miguelzackk <mzsilvaw@gmail.com>
2026-05-21 09:11:22 -0300

    wip: add Page404 in 'routes-react'

--- routes-react/src/App.jsx
+++ routes-react/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./pages/Home.jsx";
 import {Sobre} from "./pages/Sobre.jsx";
 import {Contato} from "./pages/Contato.jsx";
+import {Page404} from "./pages/Page404.jsx";
 
 function App() {
     return (<BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/sobre" element={<Sobre/>}/>
             <Route path="/contato" element={<Contato/>}/>
+            <Route path="*" element={<Page404/>}/>
         </Routes>
     </BrowserRouter>)
 }
