1492caaac867198eadff53239a16aa103ebc53ba
miguelzackk <mzsilvaw@gmail.com>
2026-05-21 10:28:08 -0300

    feat: finish 404 page in 'routes-react'

--- routes-react/src/pages/Page404/Page404.jsx
+++ routes-react/src/pages/Page404/Page404.jsx
@@ -1,11 +1,18 @@
-import {Link} from "react-router-dom";
 import "./style.scss"
+import logo from "../../assets/images/404image.png"
+import {Link} from "react-router-dom";
 
 export const Page404 = () => {
-    return (
-        <main className="page404">
-            <h1>Página 404!</h1>
-            <Link to="/">Voltar para home.</Link>
-        </main>
-    )
+    return (<main className="page404">
+        <div className="content-wrapper">
+            <img src={logo} alt="Imagem do astronauta 404"/>
+            <div className="error-link-wrapper">
+                <div className="text-wrapper">
+                    <h1 className="h1-404">OOPS!</h1>
+                    <h2>PAGE NOT FOUND</h2>
+                </div>
+                <Link to="/">GO HOME</Link>
+            </div>
+        </div>
+    </main>)
 }
