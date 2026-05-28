ffdf13cd48e34a1a4069ede371a71b2869535074
miguelzackk <mzsilvaw@gmail.com>
2026-05-28 11:06:48 -0300

    wip: started 'dragonBallAPI' project

---
+++ dragonBallAPI/src/components/card/index.jsx
@@ -0,0 +1,9 @@
+export const Card = () => {
+  return (
+      <div className="card">
+          <img src="" alt=""/>
+          <h2></h2>
+          <p></p>
+      </div>
+  )
+}
---
+++ dragonBallAPI/src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import "./style.css"
+
+export const Header = () => {
+  return (
+      <header>
+          <h1>Dragon Ball</h1>
+          <p>vamos pegar as 7 bolas do Dragão</p>
+      </header>
+  )
+}
