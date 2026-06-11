6940097f6fc2c073667ec06aad7d10961893215c
miguelzackk <mzsilvaw@gmail.com>
2026-06-11 10:59:58 -0300

    login: login service finish

--- dragonBall-React/src/App.jsx
+++ dragonBall-React/src/App.jsx
@@ -1,16 +1,36 @@
 import './App.css'
-import { Card } from './components/card'
-import { Header } from './components/header'
-import { Main } from './components/main'
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
 
-  return (
-    <>
-      <Header/>
-      <Main/>
-    </>
-  )
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+
+    const login = async () => {
+        try {
+            const resposta = await axios.post("http://localhost:3000/login", {
+                email, senha
+            })
+
+            const token = resposta.data.token
+
+            localStorage.setItem("token", token)
+            alert("Login realizado")
+        }
+        catch {
+            alert("Login inválido")
+        }
+    }
+
+    return (<>
+        <h2>Login</h2>
+        <input type="email" placeholder="Informe o seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" placeholder="Informe sua senha" value={senha}
+               onChange={(e) => setSenha(e.target.value)}/>
+        <button onClick={login}>Entrar</button>
+    </>)
 }
 
 export default App
